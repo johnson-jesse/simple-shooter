7a2ada964928ea7bbb0307874062bc2addd003d4
Jesse A. Johnson <jesseajohnson@pm.me>
2024-07-13 17:11:29 -0400

    Lesson - 189 Flush out and test custom BT Task

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Gun.h"
 #include "ShooterCharacter.h"
+#include "Gun.h"
 
 // Sets default values
 AShooterCharacter::AShooterCharacter()
