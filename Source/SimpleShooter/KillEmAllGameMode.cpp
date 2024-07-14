a2483b51b3ec2a8c561d6a558682daab1ab1f859
Jesse A. Johnson <jesseajohnson@pm.me>
2024-07-14 11:42:15 -0400

    Lesson - 197 Iterating over actors

--- Source/SimpleShooter/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -3,10 +3,21 @@
 
 #include "KillEmAllGameMode.h"
 
+#include "EngineUtils.h"
+
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
 	Super::PawnKilled(PawnKilled);
 	UE_LOG(LogTemp, Warning, TEXT("A pawn was killed"));
 	APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
 	if (PlayerController != nullptr) PlayerController->GameHasEnded(nullptr, false);
+}
+
+void AKillEmAllGameMode::EndGame(const bool bIsPlayerWinner) const
+{
+	for (AController* Controller : TActorRange<AController>(GetWorld()))
+	{
+		bool bIsWinner = Controller->IsPlayerController() == bIsPlayerWinner;
+		Controller->GameHasEnded(Controller->GetPawn(), bIsPlayerWinner);
+	}
 }
--- Source/SimpleShooter/KillEmAllGameMode.h
+++ Source/SimpleShooter/KillEmAllGameMode.h
@@ -15,4 +15,7 @@ class SIMPLESHOOTER_API AKillEmAllGameMode : public ASimpleShooterGameMode
 	GENERATED_BODY()
 public:
 	virtual void PawnKilled(APawn* PawnKilled) override;
+
+private:
+	void EndGame(bool bIsPlayerWinner) const;
 };
