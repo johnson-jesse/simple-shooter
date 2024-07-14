ebdf9beca445fab770581dd416f33fcff3fb4547
Jesse A. Johnson <jesseajohnson@pm.me>
2024-07-14 11:26:22 -0400

    Lesson - 196 Displaying a lose screen

--- Source/SimpleShooter/ShooterPlayerController.cpp
+++ Source/SimpleShooter/ShooterPlayerController.cpp
@@ -2,10 +2,15 @@
 
 
 #include "ShooterPlayerController.h"
+#include "Blueprint/UserWidget.h"
 
 void AShooterPlayerController::GameHasEnded(AActor* EndGameFocus, bool bIsWinner)
 {
 	Super::GameHasEnded(EndGameFocus, bIsWinner);
-	UE_LOG(LogTemp, Warning, TEXT("We have finished"));
+	UUserWidget* LoseScreen = CreateWidget(this, LoseScreenClass);
+	if (LoseScreen != nullptr)
+	{
+		LoseScreen->AddToViewport();
+	}
 	GetWorldTimerManager().SetTimer(RestartTimer, this, &APlayerController::RestartLevel, RestartDelay);
 }
